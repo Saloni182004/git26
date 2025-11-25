5303daec66c5b5e7c4a2485c0d832fa03ec470d0
Saloni <18salonid2004@gmail.com>
2025-11-25 15:21:01 +0530

    index

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ const routes=require("./routes/blog_router");
 const app=express();
 app.use(express.json);
 app.use(express.urlencoded({extended:true}));
-<<<<<<< HEAD
 app.use("/api/routes",require("./routes/user"));
-=======
 
 app.use("/api/blogs",routes);
 
->>>>>>> master
 app.get("/",(req,res)=>{
     res.json({message:"OKK"});
 })
